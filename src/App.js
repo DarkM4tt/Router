c7acc931ea27322c0200580715fec8147fd9c66f
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-08-06 21:20:08 +0530

    Added Router & Pages.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { Route, Switch, Redirect } from 'react-router-dom'
+import Welcome from './pages/Welcome'
+import Products from './pages/Products'
+import MainHeader from './components/MainHeader'
+import ProductDetail from './pages/ProductDetail'
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome" exact>
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
